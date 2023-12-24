b9c59df468566e383c21e1dec32bdf1e4f595be0
savicaAtomovic <107795916+savicaAtomovic@users.noreply.github.com>
2023-12-25 00:18:31 +0100

    Add confeti animation

--- src/app/game/missing-word/missing-word.component.ts
+++ src/app/game/missing-word/missing-word.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Subject, switchMap, takeUntil } from 'rxjs';
+import { Subject, interval, switchMap, takeUntil } from 'rxjs';
 import { GamesService } from 'src/app/games-list/games.service';
 import { Games, MissingGameConfig } from 'src/app/models/games';
 import { Language } from 'src/app/models/language';
 import { LetterGameCorrect } from 'src/app/models/letter-game-corrrect';
 import { SettingsService } from 'src/app/services/settings.service';
+import { PausableObservable, pausable } from 'rxjs-pausable';
 
 @Component({
   selector: 'app-missing-word',
@@ -24,12 +25,21 @@ export class MissingWordComponent implements OnInit, OnDestroy {
   shuffledImages: MissingGameConfig[];
   selectedSentence = 0;
 
+  pausable: PausableObservable<number>;
+
   constructor(
     private gamesService: GamesService,
     public settingsService: SettingsService
   ) {}
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.pausable = interval(800).pipe(
+      // @ts-ignore
+      pausable()
+    ) as PausableObservable<number>;
+    this.pausable.subscribe(this.shoot.bind(this));
+    this.pausable.pause();
     this.gamesService
       .getGameConfigByLocation(this.game.configLocation)
       .pipe(
@@ -47,7 +57,16 @@ export class MissingWordComponent implements OnInit, OnDestroy {
       });
   }
 
+  playFirework() {
+    this.pausable.resume();
+  }
+
+  stopFirework() {
+    this.pausable.pause();
+  }
+
   newGame(configs: MissingGameConfig[]) {
+    this.stopFirework();
     if (this.generatedNumbers.length + this.game.perPage > configs.length) {
       this.generatedNumbers = [];
     }
@@ -78,6 +97,12 @@ export class MissingWordComponent implements OnInit, OnDestroy {
       this.configArray
     );
     this.shuffledImages.forEach((i) => (i.correct = LetterGameCorrect.NONE));
+  }
+
+  gameOver() {
+    return this.configArray.every(
+      (config) => config.correct === LetterGameCorrect.CORRECT
+    );
   }
 
   onSentenceClick(sentence: MissingGameConfig) {
@@ -131,6 +156,10 @@ export class MissingWordComponent implements OnInit, OnDestroy {
       this.setCorrectSentence(LetterGameCorrect.CORRECT, sentence);
       this.setAllImagesCorrectToNone();
       this.selectedSentence = 0;
+      if (this.gameOver()) {
+        this.shoot();
+        this.playFirework();
+      }
     } else {
       updatedImage.correct = LetterGameCorrect.WRONG;
       setTimeout(() => {
@@ -168,6 +197,30 @@ export class MissingWordComponent implements OnInit, OnDestroy {
 
   getSvgPath(orderNumber: number): string {
     return `assets/icons/number-${orderNumber}.svg`;
+  }
+
+  confetti(args: any) {
+    // @ts-ignore
+    return window['confetti'].apply(this, arguments);
+  }
+
+  shoot() {
+    try {
+      this.confetti({
+        angle: this.random(60, 120),
+        spread: this.random(10, 50),
+        particleCount: this.random(40, 50),
+        origin: {
+          y: 0.6,
+        },
+      });
+    } catch (e) {
+      // noop, confettijs may not be loaded yet
+    }
+  }
+
+  random(min: number, max: number) {
+    return Math.random() * (max - min) + min;
   }
 
   ngOnDestroy(): void {
