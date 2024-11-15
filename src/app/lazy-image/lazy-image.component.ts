faa3ddfef8d462270e3cdcb7e4bf4f6a2b3bb000
Savica Tomovic <savica.tomovic@logate.com>
2024-11-15 23:52:56 +0100

    Use lazy image in practical lesson component

--- src/app/lazy-image/lazy-image.component.ts
+++ src/app/lazy-image/lazy-image.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrl: './lazy-image.component.scss',
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
   @Input() src: string;
   @Input() alt: string = '';
   @Input() clickable: boolean = false;
+  @Input() heightVar: string = '--image-height';
   @Output() clicked = new EventEmitter<void>();
 
   imageLoaded = false;
+
+  ngOnInit() {
+    document.documentElement.style.setProperty(
+      '--height-var',
+      `var(${this.heightVar})`
+    );
+  }
 
   onImageLoad() {
     this.imageLoaded = true;
